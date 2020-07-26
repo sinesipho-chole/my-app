905264a07c669729946a90954912203d4edb287d
Sinesipho Chole <s.chole@pathfinderza.app>
2020-07-26 17:18:26 +0200

    First commit

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
